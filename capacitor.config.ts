5a8c43953e8698d694c2d2437cdf0853daf49b2a
Mathew Simpson <mattypattysapps@zo.computer>
2026-05-21 19:30:43 +0000

    Rename to Iron Citadel Quest - updated app ID and name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mattypattysapps.nexusdefense2',
-  appName: 'Nexus Defense 2',
+  appId: 'com.mattypattysapps.ironcitadelquest',
+  appName: 'Iron Citadel Quest',
   webDir: 'dist'
 };
 
